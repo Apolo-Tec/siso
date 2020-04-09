55ca87a6d8d27b465368f3441e00f811e224e780
Felipe Oliveira <fr.oliveira94@gmail.com>
2020-04-09 18:06:43 -0300

    feat: theme

---
+++ src/theme/colors.ts
@@ -0,0 +1,83 @@
+export default {
+  dodgerBlue: '#4D7EFE',
+  white: '#ffffff',
+};
+
+export interface ColorPropsBackground {
+  /** Primary is: bostonBlue... */
+  primary: string;
+  secondary: string;
+  tertiary: string;
+  fourth: string;
+  modal: string;
+  lightBlue: string;
+  lightRed: string;
+  darkBlue: string;
+  progressBar: string;
+  corduroy: string;
+  error: string;
+  dotIndicator: string;
+}
+
+export type ButtonColorScheme = {
+  borderColor?: string;
+  backgroundColor: string;
+  fontColor: string;
+};
+
+export interface ColorsPropsButtonFlat {
+  primary: ButtonColorScheme;
+  secondary: ButtonColorScheme;
+  disabled: ButtonColorScheme;
+}
+
+export interface ColorsPropsButtonOutline {
+  primary: ButtonColorScheme;
+  disabled: ButtonColorScheme;
+}
+
+export interface ColorsPropsText {
+  primary: string;
+  secondary: string;
+  tertiary: string;
+  fourth: string;
+  darkBlue: string;
+  darkRed: string;
+  corduroy: string;
+  white: string;
+  recharge: string;
+  externalLink: string;
+}
+
+export interface ColorsPropsInformation {
+  error: string;
+  success: string;
+}
+
+export interface ColorPropsInput {
+  border: string;
+}
+
+export interface ColorsProps {
+  background: ColorPropsBackground;
+  text: ColorsPropsText;
+  information: ColorsPropsInformation;
+  input: ColorPropsInput;
+  border: string;
+  button: {
+    flat: ColorsPropsButtonFlat;
+    outline: ColorsPropsButtonOutline;
+  };
+}
+
+export type ColorsButtonFlatOptions = keyof ColorsPropsButtonFlat;
+
+export type ColorsButtonOutlineOptions = keyof ColorsPropsButtonOutline;
+
+export type ColorsBackgroundOptions = keyof ColorPropsBackground;
+
+export type ColorsTextOptions = keyof ColorsPropsText;
+
+export type ColorsInformationOptions = keyof ColorsPropsInformation;
+
+export type ColorsInputOptions = keyof ColorPropsInput;
---
+++ src/theme/index.ts
@@ -0,0 +1,36 @@
+import 'styled-components/native';
+import { DefaultTheme } from 'styled-components/native';
+
+import colors, { ColorsProps } from './colors';
+import units, { UnitsProps } from './units';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: ColorsProps;
+    units: UnitsProps;
+  }
+}
+
+export const sisoTheme: DefaultTheme = {
+  colors: {
+    background: {
+      primary: colors.dodgerBlue,
+      white: colors.white,
+    },
+  },
+  units: {
+    spacing: {
+      none: units.spacing.none,
+      xxs: units.spacing.xxs,
+      extraSmall: units.spacing.extraSmall,
+      small: units.spacing.small,
+      medium: units.spacing.medium,
+      large: units.spacing.large,
+      xlarge: units.spacing.xlarge,
+      xxlarge: units.spacing.xxlarge,
+      xxxlarge: units.spacing.xxxlarge,
+      xxxxlarge: units.spacing.xxxxlarge,
+      extraLarge: units.spacing.extraLarge,
+    },
+  },
+}
